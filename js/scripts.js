5e7357e5251f4653521a68579a862de4b8cd945a
Ginger Lee Kretschmer and Zachary Marston <zachary@>
2017-10-19 09:52:36 -0700

    allow all buttons to show information on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
-  var slugClicked =  $("#slugbtn").click(function(event){
-    event.preventDefault();
+  var slugClicked =  $("#slugbtn").click(function() {
 
   if (slugClicked){
     $("#slug").toggle();
+    // $("#sloth #sealion").hide();
   }
   });
+
+  var slothClicked =  $("#slothbtn").click(function(){
+
+  if (slothClicked){
+    $("#sloth").toggle();
+    // $("#slug #sealion").hide();
+  }
+  });
+
+  var sealionClicked =  $("#sealionbtn").click(function(){
+
+  if (sealionClicked){
+    $("#sealion").toggle();
+    // $("#slug #sloth").hide();
+  }
+  });
+
 });
