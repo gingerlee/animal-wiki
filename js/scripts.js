8a9bf6ff7e925d4e0f15bf36c90190510cc430cb
Ginger Lee Kretschmer and Zachary Marston <zachary@>
2017-10-19 10:45:47 -0700

    update javascript variables to add in booleans

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
   var slugClicked =  $("#slugbtn").click(function() {
-
-  if (slugClicked){
-    $("#slug").toggle();
-    $("#sloth").hide();
-    $("#sealion").hide();
-  }
+    var slugOn = true
+      if (slugOn === true){
+        $("#slug").toggle();
+        $("#sloth").hide();
+        $("#sealion").hide();
+      }
   });
 
   var slothClicked =  $("#slothbtn").click(function(){
-
-  if (slothClicked){
-    $("#sloth").toggle();
-    $("#slug").hide();
-    $("#sealion").hide();
-  }
+    var slothOn = true
+      if (slothOn === true){
+        $("#sloth").toggle();
+        $("#slug").hide();
+        $("#sealion").hide();
+      }
   });
 
   var sealionClicked =  $("#sealionbtn").click(function(){
-
-  if (sealionClicked){
-    $("#sealion").toggle();
-    $("#sloth").hide();
-    $("#slug").hide();
-  }
+    var  sealionOn = true
+      if (sealionOn === true){
+        $("#sealion").toggle();
+        $("#sloth").hide();
+        $("#slug").hide();
+      }
   });
 
 });
